5a28c410bbc52617223fab1a533cb681dd39b0c9
WingYeeNg <wyng0@our.ecu.edu.au>
2021-09-23 08:54:53 +0800

    Update checkpw.sh

--- checkpw.sh
+++ checkpw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ # use awk to scan all password with 3 conditions: 1/contains at least 1 uppercase 2/ contains at least 1 digit 3/ contains 8 wordcount
+ # then print only the password coloum($2) and show which meet or do not meet all requirement
+
  awk 'NR>1 { if ( $2 ~/[[:upper:]]+/ && $2 ~/[[:digit:]]+/ && $2 %8s )
              { print $2 "\t", "- meets password strength requirement" }
             else
